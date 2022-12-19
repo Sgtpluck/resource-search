4d9d21b60861440d73d7784643f5d8950c65c5c4
Davida Marion <davida.marion@gsa.gov>
2022-12-19 11:34:27 -0500

    refactor js

--- app/javascript/all-handling.js
+++ app/javascript/all-handling.js
@@ -1,35 +1,21 @@
-// const handleAll = (all_id, input_class) => {
-//   const all = document.getElementById(
-// }
-const chooseTypeAll = document.getElementById("search_query_type_All");
-const typeInputArray = Array.from(document.getElementsByClassName("type"));
-chooseTypeAll.addEventListener('click', (event) => {
-  if (event.target.value === "1") {
-    typeInputArray.forEach(input => {
-      input.checked = 0;
-    });
-  }
-});
+const handleAll = (allId, inputClass) => {
+  const all = document.getElementById(allId);
+  const inputArray = Array.from(document.getElementsByClassName(inputClass));
 
-typeInputArray.forEach(input => {
-  input.addEventListener("click", () => {
-    chooseTypeAll.checked = false; 
+  all.addEventListener('click', (event) => {
+    if (event.target.value === "1") {
+      inputArray.forEach(input => {
+        input.checked = 0;
+      });
+    }
+  });
+
+  inputArray.forEach(input => {
+    input.addEventListener("click", () => {
+      all.checked = false; 
+    })
   })
-})
+}
 
-
-const chooseSourceAll = document.getElementById("search_source_All");
-const sourceInputArray = Array.from(document.getElementsByClassName("source"));
-chooseSourceAll.addEventListener('click', (event) => {
-  if (event.target.value === "1") {
-    sourceInputArray.forEach(input => {
-      input.checked = 0;
-    });
-  }
-});
-
-sourceInputArray.forEach(input => {
-  input.addEventListener("click", () => {
-    chooseSourceAll.checked = false; 
-  })
-})
+handleAll("search_query_type_All", "type");
+handleAll("search_source_All", "source")
